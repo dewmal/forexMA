822d170e489b834a0ce41a1614afdb8d4e41405a
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-29 03:49:52 +0530

    Update price reading agent to auto heal on errors

--- agent/price_reading_agent.py
+++ agent/price_reading_agent.py
@@ -6,6 +6,7 @@ import websockets
 from agent import Agent
 from data.data_formats import MarketStatus
 from utils.binary_api_helpers import convert_message_to_environment
+from utils.web_socket import websocket_connect
 
 log = logging.getLogger(Agent.Price_Reading_Agent)
 
@@ -17,13 +18,35 @@ def print(*args, **kwargs):
 class PriceReadingAgent:
     publish = None
     display = None
+    last_candle: MarketStatus = None
 
     def __init__(self, *args, **kwargs):
         self.__binary_host = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
 
     async def start(self):
-        # await self.publish("AgentTwo", "Hi Agent 2")
-        pass
+        await websocket_connect(self.__binary_host, {
+            "ticks_history": "frxEURUSD",
+            "adjust_start_time": 1,
+            "count": 50,
+            "end": "latest",
+            "start": 1,
+            "style": "candles",
+            "subscribe": 1
+        }, self.process_message)
+
+    async def process_message(self, message):
+        price_status_list = convert_message_to_environment(message)
+        for status in price_status_list:
+            if not self.last_candle:
+                self.last_candle = status
+
+            if self.last_candle.time_stamp < status.time_stamp:
+                await self.publish(Agent.Quantitative_FAAgent, self.last_candle)
+                await self.publish(Agent.Performance_Analysing_Agent, self.last_candle)
+                await self.publish(Agent.Decision_Agent, self.last_candle)
+                await self.display(self.last_candle.to_dict())
+
+            self.last_candle = status
 
     async def accept_message(self, agent, message):
         pass
@@ -32,28 +55,16 @@ class PriceReadingAgent:
         pass
 
     async def execute(self, *args, **kwargs):
-        async with websockets.connect(self.__binary_host) as websocket:
-            await websocket.send(json.dumps({
-                "ticks_history": "frxEURUSD",
-                "adjust_start_time": 1,
-                "count": 50,
-                "end": "latest",
-                "start": 1,
-                "style": "candles",
-                "subscribe": 1
-            }))
-
-            last_candle: MarketStatus = None
-            async for message in websocket:
-                price_status_list = convert_message_to_environment(message)
-                for status in price_status_list:
-                    if last_candle == None:
-                        last_candle = status
-
-                    if last_candle.time_stamp < status.time_stamp:
-                        await self.publish(Agent.Quantitative_FAAgent, last_candle)
-                        await self.publish(Agent.Performance_Analysing_Agent, last_candle)
-                        await self.publish(Agent.Decision_Agent, last_candle)
-                        await self.display(last_candle.to_dict())
-
-                    last_candle = status
+        pass
+        # async with websockets.connect(self.__binary_host) as websocket:
+        #     await websocket.send(json.dumps({
+        #         "ticks_history": "frxEURUSD",
+        #         "adjust_start_time": 1,
+        #         "count": 50,
+        #         "end": "latest",
+        #         "start": 1,
+        #         "style": "candles",
+        #         "subscribe": 1
+        #     }))
+        #
+        #     async for message in websocket:
---
+++ utils/web_socket.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import traceback
+
+import websockets
+
+log = logging.getLogger("WEBSOCKET_HELPER")
+
+
+def log_app_error(e: BaseException, level=logging.ERROR) -> None:
+    e_traceback = traceback.format_exception(e.__class__, e, e.__traceback__)
+    traceback_lines = []
+    for line in [line.rstrip('\n') for line in e_traceback]:
+        traceback_lines.extend(line.splitlines())
+    log.log(level, traceback_lines.__str__())
+
+
+async def websocket_connect(uri, payload, consumer):
+    websocket = await websockets.connect(uri, ssl=True)
+    if payload:
+        await websocket.send(json.dumps(payload))
+    while True:
+        if not websocket.open:
+            try:
+                log.info('Websocket is NOT connected. Reconnecting...')
+                websocket = await websockets.connect(uri, ssl=True)
+                await websocket.send(payload)
+            except Exception as e:
+                log_app_error(e)
+                log.info('Unable to reconnect, trying again.')
+        try:
+            async for message in websocket:
+                if message is not None:
+                    try:
+                        await consumer(message)
+                    except Exception as e:
+                        log_app_error(e)
+        except Exception as e:
+            log_app_error(e)
+            log.info('Error receiving message from websocket.')
