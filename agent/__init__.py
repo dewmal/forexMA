044ed9730e8080dd8f6c7c21dce256ad3d8070c0
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-28 18:58:32 +0530

    Create Agents

---
+++ agent/__init__.py
@@ -0,0 +1,7 @@
+class Agent:
+    News_Reading_Agent = "NewsReadingAgent"
+    Price_Reading_Agent = "PriceReadingAgent"
+    Quantitative_FAAgent = "QuantitativeFAAgent"
+    Qualitative_FAAgent = "QualitativeFAAgent"
+    Performance_Analysing_Agent = "PerformanceAnalysingAgent"
+    Decision_Agent = "DecisionAgent"
---
+++ agent/qualitative_facts_analysing.py
@@ -0,0 +1,35 @@
+import asyncio
+
+import logging
+
+from agent import Agent
+
+log = logging.getLogger(Agent.Qualitative_FAAgent)
+
+
+def print(*args, **kwargs):
+    log.info(f"{args=}")
+
+
+class QualitativeFAAgent:
+    publish = None
+    display = None
+
+    def __init__(self, *args, **kwargs):
+        log.info(f"{self} Start")
+
+    async def start(self):
+        # await self.publish("AgentTwo", "Hi Agent 2")
+        pass
+
+    async def accept_message(self, agent, message):
+        pass
+
+    async def stop(self, *args, **kwargs):
+        pass
+
+    async def execute(self, *args, **kwargs):
+        while True:
+            print("run")
+            # await self.publish("AgentTwo", "Hello AGENT 2")
+            await asyncio.sleep(2)
