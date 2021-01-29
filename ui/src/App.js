9d6e9647caf3738a05710b79663707939593f380
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-01-29 21:33:29 +0530

    Update UI for candlestick chart

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,7 +7,7 @@ import ActionViewComponent from './components/action_view_component';
 import MarketPriceViewCompoennt from "./components/market_status_price_view_component";
 import MarketNewsViewCompoennt from "./components/market_status_news_view_component";
 import MarketQualitativeFactViewCompoennt from "./components/market_qualitative_facts_view_component";
-import MarketQuantitativeFactViewCompoennt  from "./components/market_quantitative_facts_view_component";
+import MarketQuantitativeFactViewCompoennt from "./components/market_quantitative_facts_view_component";
 
 class App extends Component {
   componentWillMount() {
@@ -16,12 +16,34 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container lg h-screen bg-black text-white">
-        <ActionViewComponent />
-        <MarketPriceViewCompoennt />
-        <MarketNewsViewCompoennt />
-        <MarketQuantitativeFactViewCompoennt />
-        <MarketQualitativeFactViewCompoennt/>
+      <div className="container lg h-screen bg-black text-white font-mono">
+        <div className="grid grid-cols-3">
+
+          <div className="col-span-2 gap-4">
+            <MarketPriceViewCompoennt />
+          </div>
+          <div>
+            <ActionViewComponent />
+          </div>
+          <div className="col-span-3">
+            <MarketNewsViewCompoennt />
+          </div>
+          <div>
+            <MarketQuantitativeFactViewCompoennt />
+          </div>
+          <div>
+            <MarketQualitativeFactViewCompoennt />
+          </div>
+
+          <div>
+            <MarketQualitativeFactViewCompoennt />
+          </div>
+
+
+
+        </div>
+
+
       </div>
 
     );
--- ui/src/components/market_status_price_view_component.js
+++ ui/src/components/market_status_price_view_component.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import Chart from "react-google-charts";
 
 
 
 const MarketPriceViewCompoennt = ({ prices }) => {
 
-	return (<> Price Variations
-	
+	return (<div className="p-4">
+
+		<h1 className="text-3xl">Market Status</h1>
+
 		<ul>
 			<li>Name, Time, Open, High, Low, Close, Volume</li>
 			{prices.map((p) => <li>
@@ -23,7 +25,25 @@ const MarketPriceViewCompoennt = ({ prices }) => {
 			</li>)}
 		</ul>
 
-	</>);
+		<Chart
+			width={'100%'}
+			height={350}
+			chartType="CandlestickChart"
+			loader={<div>Loading Chart</div>}
+			data={[
+				['time', 'open', 'high', 'low', 'close'],
+				...prices.map((d) => ["" + new Date(d.time_stamp * 1000).toLocaleString(), d.low, d.open, d.close, d.high])
+			]}
+			options={{
+				legend: 'none',
+				candlestick: {
+					fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
+					risingColor: { strokeWidth: 0, fill: '#0f9d58' }, // green
+				},
+			}}
+			rootProps={{ 'data-testid': '1' }}
+		/>
+	</div>);
 }
 
 export default connect((state) => {
