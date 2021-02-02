1258eba85bbb209125f26146c3f9c2d021275e36
Dewmal Nilanka <dewmalnilanka@gmail.com>
2021-02-02 22:58:49 +0530

    Update market trend table

--- ui/src/components/market_trend_view_component.js
+++ ui/src/components/market_trend_view_component.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+const TrendTableView = ({ trends }) => {
+	return (<>
+		<table class="table-auto w-full text-sm text-left">
+			<thead>
+				<tr>
+					<th>Asset</th>
+					<th>Change</th>
+					<th>Lot</th>
+				</tr>
+			</thead>
+			<tbody>
+				{trends.map((trend) =>
+
+					<tr>
+						<td><a target="blank" href={"https://www.binance.com/en/trade/" + trend.name + "?layout=pro"}>{trend.name}</a></td>
+						<td>{trend.change}</td>
+						<td>{trend.length}</td>
+					</tr>
+
+				)}
+
+			</tbody>
+		</table>
+
+
+
+
+
+	</>);
+}
+
 const MarketTrendViewComponent = ({ trends }) => {
 	return (
-		<>
+		<div className="p-2">
 			Market Trend
 			<ul>
-				{(trends.map((t) => {
+				<TrendTableView trends={(trends.map((t) => {
 					return {
 						...t,
-						change: Math.round(t.change * 10000)/100
+						change: Math.round(t.change * 10000) / 100
 					}
-				}).sort((a,b)=>b.change-a.change)).map((trend) => <li>
-					<a target="blank" href={"https://www.binance.com/en/trade/"+trend.name+"?layout=pro"}>{trend.name}</a>,
-					
-					{trend.change},{trend.length}</li>)}
+				}).sort((a, b) => b.change - a.change))} />
 			</ul>
-		</>
+		</div>
 	);
 }
 
